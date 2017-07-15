8bbbeb5f036a815c770a8d26b0bf7202b58e4f14
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2017-07-15 20:43:50 +0200

    header as htmlDependency

--- R/mathjax.r
+++ R/mathjax.r
@@ -1,10 +1,21 @@
+examples.mathjaxlocal = function() {
+  library(shinyEvents)
+  app = eventsApp()
+  app$ui = fluidPage(
+    with_mathjax(p("\\(x^2\\)"))
+  )
+  viewApp(app)
+}
 mathjaxLocalHeader = function() {
 
   dir = system.file("www/MathJax-2.7.1",package="MathjaxLocal")
-  shiny::addResourcePath("RMathjaxLocal",dir)
+  #shiny::addResourcePath("RMathjaxLocal",dir)
+  #path <- "RMathjaxLocal/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+  #tags$head(singleton(tags$script(src = path, type = "text/javascript")))
 
-  path <- "RMathjaxLocal/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
-  tags$head(singleton(tags$script(src = path, type = "text/javascript")))
+
+  htmlDependency("RMathjaxLocal",version="2.7.1", src=c(file=dir), head ='<script src="RMathjaxLocal-2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>')
+
 }
 
 
